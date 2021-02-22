2b3c34776b627d4a179513dee5ad28955b27adf4
MarwanReyadAL-Khawaldeh <marwanreyad@gmail.com>
2021-02-22 14:21:44 +0200

    Finished

--- js/sales.js
+++ js/sales.js
@@ -1,15 +1,16 @@
 'use strict';
+let hour = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 console.log('hi');
 const Seattle = {
   name: 'Seattle',
-  hour: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+
   Min: 23,
   Max: 65,
   Avg: 6.3,
   arr: [],
-  num: [],
+  Total: 0,
   avgCookies: 0,
-  getAvg: function (avg) {
+  getAvg: function () {
     this.avgCookies = this.Avg * Math.floor(getRandom(this.Min, this.Max));
 
   },
@@ -20,30 +21,34 @@ const Seattle = {
     h1Element.textContent = this.name;
     const ulElement = document.createElement('ul');
     parentElement.appendChild(ulElement);
-    for (let i = 0; i < this.hour.length; i++) {
+    for (let i = 0; i < hour.length; i++) {
       const liElement = document.createElement('li');
       ulElement.appendChild(liElement);
-      liElement.textContent = (`${this.hour[i]}   : ${this.avgCookies} Cookies`);
+      this.avgCookies = (Math.ceil(getRandom(this.Min, this.Max) * this.Avg));
+      this.arr.push(this.avgCookies);
+      this.Total += this.arr[i];
+
+      liElement.textContent = (`${hour[i]}   : ${this.avgCookies} Cookies`);
     }
-    for (let i = 0; i < this.hour; i++) {
-      this.arr.push(Math.floor (this.avgCookies));
-    }
+    console.log(this.arr);
     const liElement = document.createElement('li');
     ulElement.appendChild(liElement);
-    liElement.textContent = (`Totel  ${this.arr} Cookies`);
-  
+    liElement.textContent = (`Total  ${this.Total}  Cookies`);
+
 
   }
 };
 
 const Tokyo = {
   name: 'Tokyo',
-  hour: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   Min: 3,
   Max: 24,
   Avg: 1.2,
+  arr: [],
+  Total: 0,
+
   avgCookies: 0,
-  getAvg: function (avg) {
+  getAvg: function () {
     this.avgCookies = this.Avg * Math.floor(getRandom(this.Min, this.Max));
   },
   render: function () {
@@ -53,23 +58,35 @@ const Tokyo = {
     h1Element.textContent = this.name;
     const ulElement = document.createElement('ul');
     parentElement.appendChild(ulElement);
-    for (let i = 0; i < this.hour.length; i++) {
+    for (let i = 0; i < hour.length; i++) {
       const liElement = document.createElement('li');
       ulElement.appendChild(liElement);
-      liElement.textContent = (`${this.hour[i]}   : ${this.avgCookies} Cookies`);
+      this.avgCookies = (Math.ceil(getRandom(this.Min, this.Max) * this.Avg));
+      this.arr.push(this.avgCookies);
+      this.Total += this.arr[i];
+
+      liElement.textContent = (`${hour[i]}   : ${this.avgCookies} Cookies`);
     }
+    console.log(this.arr);
+    const liElement = document.createElement('li');
+    ulElement.appendChild(liElement);
+    liElement.textContent = (`Total  ${this.Total}  Cookies`);
+
+
   }
 };
 
 
 const Dubai = {
   name: 'Dubai',
-  hour: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   Min: 11,
   Max: 38,
   Avg: 3.7,
+  arr: [],
+  Total: 0,
+
   avgCookies: 0,
-  getAvg: function (avg) {
+  getAvg: function () {
     this.avgCookies = this.Avg * Math.floor(getRandom(this.Min, this.Max));
   },
   render: function () {
@@ -79,22 +96,33 @@ const Dubai = {
     h1Element.textContent = this.name;
     const ulElement = document.createElement('ul');
     parentElement.appendChild(ulElement);
-    for (let i = 0; i < this.hour.length; i++) {
+    for (let i = 0; i < hour.length; i++) {
       const liElement = document.createElement('li');
       ulElement.appendChild(liElement);
-      liElement.textContent = (`${this.hour[i]}   :   ${this.avgCookies} Cookies`);
+      this.avgCookies = (Math.ceil(getRandom(this.Min, this.Max) * this.Avg));
+      this.arr.push(this.avgCookies);
+      this.Total += this.arr[i];
+
+      liElement.textContent = (`${hour[i]}   : ${this.avgCookies} Cookies`);
     }
+    console.log(this.arr);
+    const liElement = document.createElement('li');
+    ulElement.appendChild(liElement);
+    liElement.textContent = (`Total  ${this.Total}  Cookies`);
+
+
   }
 };
 
 const Paris = {
   name: 'Paris',
-  hour: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   Min: 20,
   Max: 38,
   Avg: 2.3,
+  arr: [],
+  Total: 0,
   avgCookies: 0,
-  getAvg: function (avg) {
+  getAvg: function () {
     this.avgCookies = this.Avg * Math.floor(getRandom(this.Min, this.Max));
   },
 
@@ -105,22 +133,32 @@ const Paris = {
     h1Element.textContent = this.name;
     const ulElement = document.createElement('ul');
     parentElement.appendChild(ulElement);
-    for (let i = 0; i < this.hour.length; i++) {
+    for (let i = 0; i < hour.length; i++) {
       const liElement = document.createElement('li');
       ulElement.appendChild(liElement);
-      liElement.textContent = (`${this.hour[i]}   :   ${this.avgCookies} Cookies`);
+      this.avgCookies = (Math.ceil(getRandom(this.Min, this.Max) * this.Avg));
+      this.arr.push(this.avgCookies);
+      this.Total += this.arr[i];
+      liElement.textContent = (`${hour[i]}   : ${this.avgCookies} Cookies`);
     }
+    console.log(this.arr);
+    const liElement = document.createElement('li');
+    ulElement.appendChild(liElement);
+    liElement.textContent = (`Total  ${this.Total}  Cookies`);
+
+
   }
 };
 
 const Lima = {
   name: 'Lima',
-  hour: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   Min: 2,
   Max: 16,
   Avg: 4.6,
   avgCookies: 0,
-  getAvg: function (avg) {
+  Total: 0,
+  arr: [],
+  getAvg: function () {
     this.avgCookies = this.Avg * Math.floor(getRandom(this.Min, this.Max));
   },
   render: function () {
@@ -130,11 +168,21 @@ const Lima = {
     h1Element.textContent = this.name;
     const ulElement = document.createElement('ul');
     parentElement.appendChild(ulElement);
-    for (let i = 0; i < this.hour.length; i++) {
+    for (let i = 0; i < hour.length; i++) {
       const liElement = document.createElement('li');
       ulElement.appendChild(liElement);
-      liElement.textContent = (`${this.hour[i]}   :   ${this.avgCookies} Cookies`);
+      this.avgCookies = (Math.ceil(getRandom(this.Min, this.Max) * this.Avg));
+      this.arr.push(this.avgCookies);
+      this.Total += this.arr[i];
+
+      liElement.textContent = (`${hour[i]}   : ${this.avgCookies} Cookies`);
     }
+    console.log(this.arr);
+    const liElement = document.createElement('li');
+    ulElement.appendChild(liElement);
+    liElement.textContent = (`Total  ${this.Total}  Cookies`);
+
+
   }
 };
 
